2c15acc481812ae3299b8d6c4865cf231a71cd0b
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2022-05-10 14:41:07 +0600

    Automatic configuration with django-on-heroku

--- azim_site/azim_site/settings.py
+++ azim_site/azim_site/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import django_on_heroku
+django_on_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -29,7 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 ###########################################################################################
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
